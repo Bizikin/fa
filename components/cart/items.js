fcb7117d1e86f220aedecd6148809d35910e4a03
Yevgeniy343 <rt4387@yahoo.com>
2024-03-01 12:25:26 +0300

    test

--- components/cart/items.js
+++ components/cart/items.js
@@ -4,11 +4,22 @@ import { useSelector, useDispatch } from "react-redux";
 import Item from "../../components/cart/item";
 import _ from "lodash";
 import Checkbox from "./checkbox";
+import { Toaster, toaast } from "react-hot-toast";
+
+const initialState = {
+  customerName: "",
+  phoneNumber: "",
+  address: "",
+};
 
 const Items = () => {
   const { basket } = useSelector((store) => store.user);
   const [payment, setPayment] = useState("");
-  console.log(payment);
+  const [values, setValues] = useState(initialState);
+
+  const changeHandler = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
   const backet2 = _.map(basket, (b) => ({
     ...b,
@@ -25,6 +36,13 @@ const Items = () => {
     setPayment(state);
   };
 
+  const submitHandler = () => {
+    const { customerName, phoneNumber, address } = values;
+    console.log("address", address);
+    console.log("customerName", customerName);
+    console.log("phoneNumber", phoneNumber);
+  };
+
   return (
     <Wrapper>
       {basket?.map((b, index) => (
@@ -36,10 +54,28 @@ const Items = () => {
       <div className="anketa">
         <div className="left">
           <p className="header">ЗАПОЛНИТЕ ДАННЫЕ</p>
-          <input type="text" placeholder="Имя" />
+          <input
+            type="text"
+            placeholder="Имя"
+            name="customerName"
+            value={values.customerName}
+            onChange={changeHandler}
+          />
           <p className="red">Поле обязательно</p>
-          <input type="text" placeholder="+7 (" />
-          <input type="text" placeholder="Адрес" />
+          <input
+            type="text"
+            placeholder="+7 ("
+            name="phoneNumber"
+            value={values.phoneNumber}
+            onChange={changeHandler}
+          />
+          <input
+            type="text"
+            placeholder="Адрес"
+            name="address"
+            value={values.address}
+            onChange={changeHandler}
+          />
         </div>
         <div className="right">
           <p className="header">СПОСОБ ОПЛАТЫ</p>
@@ -59,8 +95,27 @@ const Items = () => {
             status={payment !== "БАНКОВСКОЙ КАРТОЙ КУРЬЕРУ" ? false : true}
           />
         </div>
-        <div className="right"></div>
       </div>
+      <div className="actions">
+        <button className="btn" onClick={submitHandler}>
+          ОФОРМИТЬ ЗАКАЗ
+        </button>
+      </div>
+      <Toaster
+        position={"top-center"}
+        reverseOrder={false}
+        toastOptions={{
+          duration: 5000,
+          style: {
+            padding: "1rem",
+            // backgroundColor: "#ffdfff",
+            fontSize: "1rem",
+            // color: "#17132a",
+            // borderColor: "#ff00ff",
+            // border: "2px solid #ff00ff",
+          },
+        }}
+      />
     </Wrapper>
   );
 };
@@ -91,8 +146,7 @@ const Wrapper = styled.div`
   .anketa {
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-
+    margin: 20px;
     .left {
       display: flex;
       flex-direction: column;
@@ -116,7 +170,31 @@ const Wrapper = styled.div`
     font-size: 16px;
     color: red;
   }
-
+  .actions {
+    display: flex;
+    justify-content: center;
+  }
+  .btn {
+    margin-bottom: 156px;
+    padding: 31px 49px;
+    box-sizing: border-box;
+    border-radius: 36.652px;
+    background: rgba(0, 0, 0, 0.45);
+    box-shadow: var(--shadow-1);
+    color: #eadbbe;
+    font-family: Inter, sans-serif;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-transform: uppercase;
+    border: none;
+    cursor: pointer;
+    transition: 0.5s;
+    &:hover {
+      box-shadow: none;
+    }
+  }
   @media (min-width: 576px) {
   }
   @media (min-width: 768px) {
@@ -124,6 +202,9 @@ const Wrapper = styled.div`
   @media (min-width: 992px) {
     .anketa {
       flex-direction: row;
+      justify-content: space-between;
+      width: 100%;
+      /* margin: 0; */
     }
   }
   @media (min-width: 1200px) {
