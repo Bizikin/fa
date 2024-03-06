40436b4fbf9204d2f9e442914d95a346c34e05d9
Yevgeniy343 <rt4387@yahoo.com>
2024-03-06 15:35:15 +0300

    test

--- utils/axios.js
+++ utils/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  // baseURL: "http://localhost:1000/api/",
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/`,
   withCredentials: true,
 });
+
+customFetch.defaults.headers.common["Content-Type"] = "application/json";
 
 customFetch.interceptors.request.use((config) => {
   return config;
