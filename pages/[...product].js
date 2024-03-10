40c04915f7ae7bc1c67d38e72ff3f637d656e713
Yevgeniy343 <rt4387@yahoo.com>
2024-03-10 19:00:13 +0300

    test

--- pages/[...product].js
+++ pages/[...product].js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import RestaurantModal from "../components/modal/RestaurantModal";
 import { useSelector } from "react-redux";
